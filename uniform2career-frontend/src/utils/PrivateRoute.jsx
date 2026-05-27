ece1087a2534527738580d1a050d470db1bdf048
Reshma Chowdary Kommi <reshmakommi18@gmail.com>
2026-05-27 17:02:24 +0530

    Delete uniform2career-frontend directory

--- uniform2career-frontend/src/utils/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { getLoggedUser } from "./auth";  // ✅ make sure path is correct
-
-export default function PrivateRoute({ children }) {
-  const user = getLoggedUser();
-  return user ? children : <Navigate to="/login" replace />;
-}
