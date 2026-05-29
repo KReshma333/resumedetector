3b801a0a84fdf16f7bfe3f9da68cac4f4d3cdfa0
Reshma Chowdary Kommi <reshmakommi18@gmail.com>
2026-05-29 15:56:34 +0530

    edited target url

--- frontend/resumedetector/vite.config.js
+++ frontend/resumedetector/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8888',
-        changeOrigin: true
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true
+        }
       }
     }
-  }
-})
+  })
+}
