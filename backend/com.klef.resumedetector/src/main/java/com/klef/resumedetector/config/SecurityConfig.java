13229e9aa679a2e5d8d2c5c59c4723a509049073
Reshma Chowdary Kommi <reshmakommi18@gmail.com>
2026-05-29 16:54:50 +0530

    changed set of allowed origins

--- backend/com.klef.resumedetector/src/main/java/com/klef/resumedetector/config/SecurityConfig.java
+++ backend/com.klef.resumedetector/src/main/java/com/klef/resumedetector/config/SecurityConfig.java
@@ -56,11 +56,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "http://localhost:3001",
-                "http://localhost:5173",
-                "http://localhost:5174",
-             "https://resumedetector-rouge.vercel.app"
+               "http://localhost:*",
+    "https://*.vercel.app"
         ));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
