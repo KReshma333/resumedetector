44bc78504593da6881d303dd6bdc03143d863d66
Reshma Chowdary Kommi <reshmakommi18@gmail.com>
2026-05-29 15:58:33 +0530

    changed base url

--- frontend/resumedetector/src/api/axiosInstance.jsx
+++ frontend/resumedetector/src/api/axiosInstance.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const API_URL = import.meta.env.VITE_API_URL
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8888',
+  baseURL: API_URL,
   headers: { 'Content-Type': 'application/json' }
 })
 
